80111247ab6a06566da196432e34f611e1386e02
TANISHQ MAHAPATRA <138215377+tanishqmahapatra@users.noreply.github.com>
2024-11-16 17:56:12 +0530

    Update redisClient.js

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,19 +1,24 @@
 const redis = require('redis');
 
-const redisClient = redis.createClient({ url: 'redis://localhost:6379' });
+// Use the REDIS_URL environment variable for the connection string
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379'; // Default to localhost if the environment variable is not set
+
+const redisClient = redis.createClient({
+  url: redisUrl,
+});
 
 redisClient.on('error', (error) => {
-    console.error('Redis connection error:', error);
+  console.error('Redis connection error:', error);
 });
 
 // Connect to Redis and export the client
 (async () => {
-    try {
-        await redisClient.connect();
-        console.log('Connected to Redis');
-    } catch (error) {
-        console.error('Failed to connect to Redis:', error);
-    }
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (error) {
+    console.error('Failed to connect to Redis:', error);
+  }
 })();
 
 module.exports = redisClient;
