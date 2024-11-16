d99bf9d7ce726ba7044fd914494a945e24949d82
TANISHQ MAHAPATRA <138215377+tanishqmahapatra@users.noreply.github.com>
2024-11-16 20:56:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,10 +81,11 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL 
+    callbackURL: 'https://mini-crm-app-backend.onrender.com/auth/google/callback',  // Should match exactly with the one in Google Console
 }, (accessToken, refreshToken, profile, done) => {
-    return done(null, profile); // This can be extended to save user info in the database if required
+    return done(null, profile); // User profile will be saved in session
 }));
+
 
 // Serialize user into the session
 passport.serializeUser((user, done) => {
