419d0a40658be6faed5c878ddc3e0aababa1141b
TANISHQ MAHAPATRA <138215377+tanishqmahapatra@users.noreply.github.com>
2024-11-16 19:27:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGO_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
